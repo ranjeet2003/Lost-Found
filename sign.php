a67ddd5429f190a33ea904ed8054bf75e064033e
ranjeet <1705213032@ietlucknow.ac.in>
2020-04-26 19:01:36 +0530

    Session is also working from signup page

--- sign.php
+++ sign.php
@@ -8,11 +8,16 @@
         $qry="INSERT INTO `info`(`uname`, `Email`, `MobileNumber`, `password`) VALUES ('$username','$email','$mobilenumber','$password')";
         $run=mysqli_query($con,$qry);
         if($run==true){
+            $qry1="SELECT `uname` FROM `info` WHERE `Email`='$email' AND `password`='$password'";
+            $run1=mysqli_query($con,$qry1);
+            $data = mysqli_fetch_assoc($run1);
             ?>
             <script>
                 alert('Sign up Succesfully')
             </script>
             <?php
+            session_start();
+            $_SESSION['username']=$data['uname'];
             header('location:user/welcome.php');
         }
     }
